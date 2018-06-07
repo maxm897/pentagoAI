bfc5709d1fd1f0bc687090aa7109017219e802ca
Max Melamed <maxm897@Gmail.com>
2018-06-07 17:10:54 -0400

    fixed minimax

--- minimax.py
+++ minimax.py
@@ -11,6 +11,21 @@ def minimax(board, depth, bestaction):
         return eval
     else:
         val = evaluate.MINVAL
+<<<<<<< HEAD
+        for x in range(0,6):
+            for y in range(0,6):
+                if isAvailable(board, x, y):
+                    for box in range(1,5):
+                        for dir in ["R", "L"]:
+                            a = GamePlay.Action(x,y,box,dir)
+                            val2 = maximin(GamePlay.take_action(board, a, "AI"), depth-1, bestaction)
+                            if (val2>val):
+                                val=val2
+                                bestaction.x_coordinate=x
+                                bestaction.y_coordinate = y
+                                bestaction.square_index=box
+                                bestaction.direction=dir
+=======
         for x in range(6):
             for y in range(6):
                 for box in range(1,5):
@@ -23,6 +38,7 @@ def minimax(board, depth, bestaction):
                             bestaction.y_coordinate = y
                             bestaction.square_index=box
                             bestaction.direction=dir
+>>>>>>> refs/remotes/origin/master
         return val
 
 
@@ -34,6 +50,17 @@ def maximin(board, depth, bestaction):
         return eval
     else:
         val = evaluate.MAXVAL
+<<<<<<< HEAD
+        for x in range(0, 6):
+            for y in range(0, 6):
+                if (isAvailable(board, x, y)):
+                    for box in range(1, 5):
+                        for dir in ["R", "L"]:
+                            a = GamePlay.Action(x, y, box, dir)
+                            val2 = minimax(GamePlay.take_action(board, a, "Player"), depth - 1, bestaction)
+                            if (val2 < val):
+                                val = val2
+=======
         for x in range(6):
             for y in range(6):
                 for box in range(1, 5):
@@ -42,6 +69,7 @@ def maximin(board, depth, bestaction):
                         val2 = minimax(GamePlay.take_action(board, a, "Player"), depth - 1, bestaction)
                         if (val2 < val):
                             val = val2
+>>>>>>> refs/remotes/origin/master
         return val
 
 
@@ -50,3 +78,8 @@ def getBestAction(board, depth):
     temp = deepcopy(board)
     minimax(temp, depth, bestaction)
     return bestaction
+
+def isAvailable(board, x, y):
+    if(board[x][y]==0 or board[x][y]==1):
+        return False
+    return True
