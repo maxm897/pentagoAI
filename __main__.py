ce3e2c4421c4b7261f782ddf5abe61b218ec2281
Karson Daecher <karsondaecher@Karsons-MacBook-Air.local>
2018-06-16 10:23:43 -0700

    minor GUI changes

--- __main__.py
+++ __main__.py
@@ -4,11 +4,11 @@ import GamePlay
 import tkinter
 
 def main():
-	print('Welcome to Pentago! The first player to get 5 marbles in a row wins the game. In each turn,',
+	print('\nWelcome to Pentago! The first player to get 5 marbles in a row wins the game. In each turn,',
 		'players place a marble in an empty circle and then rotate one of the four primary game squares',
 		'90 degrees. A turn is specified by 4 inputs: The x coordinate of the marble location (int 0...5),',
 		'the y coordinate of the marble location (int 0...5), the index of the square to be rotated',
-		'(int 1...4) and the direstion fo rotation ("R" or "L"). See the following diagrams for reference.',
+		'(int 1...4) and the direction of rotation ("R" or "L"). See the following diagrams for reference.',
 		'Good luck!',
 		'\n\nBoard Layout:\n5 \n4 \n3 \n2 \n1 \n0 \n 0 1 2 3 4 5 \n\nSquare Index Layout:\n2   4\n1   3\n')
 	new_game()
