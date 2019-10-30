7856817e53526c3bfdfea1e4c57f8416fa5ddd0c
Richard Greenbaum <rgreenbaum@dhcp-rhodes-282.eduroam.cornell.edu>
2019-10-30 10:55:22 -0400

    added richie branch with symmetric pruning

--- minimax.py
+++ minimax.py
@@ -1,6 +1,7 @@
 import evaluate
 import GamePlay
 from copy import copy, deepcopy
+import numpy as np
 
 
 bestaction = None
@@ -10,6 +11,7 @@ def minimax(board, depth, bestaction):
     if ((eval==-9999999) or (eval==9999999) or (eval==-0.5) or (depth==0)):
         return eval
     else:
+        hashes_seen = set()
         val = evaluate.MINVAL
         for x in range(0,6):
             for y in range(0,6):
@@ -18,13 +20,16 @@ def minimax(board, depth, bestaction):
                         for dir in ["R", "L"]:
                             a = GamePlay.Action(x,y,box,dir)
                             temp = deepcopy(board)
-                            val2 = maximin(GamePlay.take_action(temp, a, "AI"), depth-1, bestaction)
-                            if (val2>val):
-                                val=val2
-                                bestaction.x_coordinate=x
-                                bestaction.y_coordinate = y
-                                bestaction.square_index=box
-                                bestaction.direction=dir
+                            new_board = GamePlay.take_action(temp, a, "AI")
+                            if isUnique(symmetricBoards(new_board), hashes_seen):
+                                hashes_seen.add(hash(str(new_board)))
+                                val2 = maximin(new_board, depth-1, bestaction)
+                                if (val2>val):
+                                    val=val2
+                                    bestaction.x_coordinate=x
+                                    bestaction.y_coordinate = y
+                                    bestaction.square_index=box
+                                    bestaction.direction=dir
         return val
 
 
@@ -35,6 +40,7 @@ def maximin(board, depth, bestaction):
     if ((eval == -9999999) or (eval == 9999999) or (eval == -0.5) or (depth == 0)):
         return eval
     else:
+        hashes_seen = set()
         val = evaluate.MAXVAL
         for x in range(0, 6):
             for y in range(0, 6):
@@ -43,20 +49,22 @@ def maximin(board, depth, bestaction):
                         for dir in ["R", "L"]:
                             a = GamePlay.Action(x, y, box, dir)
                             temp = deepcopy(board)
-                            val2 = minimax(GamePlay.take_action(temp, a, "Player"), depth - 1, bestaction)
-                            if (val2 < val):
-                                val = val2
-        return val
+                            new_board = GamePlay.take_action(temp, a, "AI")
+                            if isUnique(symmetricBoards(new_board), hashes_seen):
+                                hashes_seen.add(hash(str(new_board)))
+                                val2 = minimax(GamePlay.take_action(temp, a, "Player"), depth - 1, bestaction)
+                                if (val2 < val):
+                                    val = val2
+            return val
 
 
 def getBestAction(board, depth):
     
     bestaction = findAvailableAction(board)
-    copy = GamePlay.Action(bestaction.x_coordinate, bestaction.y_coordinate, bestaction.square_index, bestaction.direction)
+    print("best action before minimax is " + str(bestaction.x_coordinate) + str(bestaction.y_coordinate))
     temp = deepcopy(board)
     minimax(temp, depth, bestaction)
-    if bestaction.equals(copy):
-        print("the AI has used an arbitrary random move")
+    print("best action after minimax is " + str(bestaction.x_coordinate) + str(bestaction.y_coordinate))
     return bestaction
 
 def isAvailable(board, x, y):
@@ -72,3 +80,35 @@ def findAvailableAction(board):
         for y in range(0,6):
             if isAvailable(board, x, y):
                 return GamePlay.Action(x, y, 1, "L")
+
+
+def symmetricBoards(board):
+    output = [board]
+
+    #rotations
+    mid_board = np.array(board)
+    for i in range(3):
+        mid_board = np.rot90(mid_board)
+        output.append(mid_board.tolist())
+
+    #horizontal and vertical flips
+    mid_board = np.array(board)
+    output.append(np.flip(mid_board, axis=0).tolist())
+    output.append(np.flip(mid_board, axis=1).tolist())
+
+    #flips across the two diagonals. Commented out because they slow the program down
+    # output.append(np.flip(np.rot90(mid_board), axis=0).tolist())
+    # output.append(np.flip(np.rot90(mid_board), axis=1).tolist())
+
+    return output
+
+def isUnique(symmetries, set):
+    for board in symmetries:
+        if hash(str(board)) in set:
+            return False
+    return True
+
+
+
+
+                
