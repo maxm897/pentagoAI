03ad4293914236d35311d7e27db8bea1a8e58fa7
rgreenbaum97 <rgreenbaum97@gmail.com>
2019-12-05 10:48:06 -0500

    commented out symmetric pruning

--- minimax.py
+++ minimax.py
@@ -11,9 +11,25 @@ def minimax(board, depth, alpha, beta, bestaction, is_top):
     initial_eval = evaluate.evaluate(board)
     if ((initial_eval==-9999999) or (initial_eval==9999999) or (initial_eval==-0.5) or (depth==0)):
         return initial_eval
+    val = evaluate.MINVAL
+    if is_top:
+        actions = generate_quick_best(board)
+        for (a, _) in actions:
+            temp = deepcopy(board)
+            new_board = GamePlay.take_action(temp, a, "AI")
+            val2 = maximin(new_board, depth-1, alpha, beta, bestaction)
+            if (val2>val):
+                val=val2
+                bestaction.x_coordinate=a.x_coordinate
+                bestaction.y_coordinate = a.y_coordinate
+                bestaction.square_index=a.square_index
+                bestaction.direction=a.direction
+            alpha = max(alpha, val)
+            if alpha >= beta:
+                return val
+        return val
     else:
         hashes_seen = set()
-        val = evaluate.MINVAL
         for x in range(0,6):
             for y in range(0,6):
                 if isAvailable(board, x, y):
@@ -27,11 +43,6 @@ def minimax(board, depth, alpha, beta, bestaction, is_top):
                                 val2 = maximin(new_board, depth-1, alpha, beta, bestaction)
                                 if (val2>val):
                                     val=val2
-                                    if is_top:
-                                        bestaction.x_coordinate=x
-                                        bestaction.y_coordinate = y
-                                        bestaction.square_index=box
-                                        bestaction.direction=direction
                                 alpha = max(alpha, val)
                                 if alpha >= beta:
                                     return val
@@ -94,19 +105,19 @@ def symmetricBoards(board):
     output = [board]
 
     #rotations
-    mid_board = np.array(board)
-    for i in range(3):
-        mid_board = np.rot90(mid_board)
-        output.append(mid_board.tolist())
+    # mid_board = np.array(board)
+    # for i in range(3):
+    #     mid_board = np.rot90(mid_board)
+    #     output.append(mid_board.tolist())
 
-    #horizontal and vertical flips
-    mid_board = np.array(board)
-    output.append(np.flip(mid_board, axis=0).tolist())
-    output.append(np.flip(mid_board, axis=1).tolist())
+    # #horizontal and vertical flips
+    # mid_board = np.array(board)
+    # output.append(np.flip(mid_board, axis=0).tolist())
+    # output.append(np.flip(mid_board, axis=1).tolist())
 
-    #flips across the two diagonals
-    output.append(np.flip(np.rot90(mid_board), axis=0).tolist())
-    output.append(np.flip(np.rot90(mid_board), axis=1).tolist())
+    # #flips across the two diagonals
+    # output.append(np.flip(np.rot90(mid_board), axis=0).tolist())
+    # output.append(np.flip(np.rot90(mid_board), axis=1).tolist())
 
     return output
 
@@ -116,6 +127,23 @@ def isUnique(symmetries, set):
             return False
     return True
 
+def generate_quick_best(board):
+    actions = []
+    hashes_seen = set()
+    for x in range(0,6):
+        for y in range(0,6):
+            if isAvailable(board, x, y):
+                for box in range(1,5):
+                    for direction in ["R", "L"]:
+                        a = GamePlay.Action(x,y,box,direction)
+                        temp = deepcopy(board)
+                        new_board = GamePlay.take_action(temp, a, "AI")
+                        if isUnique(symmetricBoards(new_board), hashes_seen):
+                            hashes_seen.add(hash(str(new_board)))
+                            initial_eval = evaluate.evaluate(board)
+                            actions.append((a, initial_eval))
+    actions.sort(key = operator.itemgetter(1), reverse=True)
+    return actions
 
 
 
