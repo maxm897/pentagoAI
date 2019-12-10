d4af381e52825b6bd5fd99fccf9662755012e741
Max Melamed <maxm897@Gmail.com>
2019-12-10 12:04:12 -0500

    added function headers for using perfect pentago

---
+++ boardToFormat.py
@@ -0,0 +1,14 @@
+import baseconvert
+
+
+def convertBoardToFormat(board):
+    # take in board in form of a 36-length ternary tuple, convert to proper form
+    bottomLeft = board[:9]
+
+
+def convertQuadrantToBinary():
+    pass
+
+
+def convertBinaryToDecimal():
+    pass
