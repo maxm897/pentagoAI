ddf0bdd1a8e6a231ad47e5e0b92e32431e56d064
Max Melamed <maxm897@Gmail.com>
2019-12-17 16:49:45 -0500

    throws exception on perfect win

--- request.py
+++ request.py
@@ -1,20 +1,22 @@
 from requests import get
 from boardToFormat import convertBoardToFormat, convertFormatToBoard
-from random import randint
+from random import choice
 
 
 def takeBoardReturnReponseBoard(board):
     # takes in a board in the form of a 36-length ternary tuple
     # outputs a board in the same format after perfect pentago makes its move
-    # print(board)
+    print(board)
     decimalBoard = convertBoardToFormat(board)
     # print(decimalBoard)
     bestMoveResponse = makeRequest(decimalBoard)
-
-    return convertFormatToBoard(bestMoveResponse)
+    c = convertFormatToBoard(bestMoveResponse)
+    print(c)
+    return c
 
 
 def makeRequest(state):
+    print(state)
     # takes in a board state as a decimal number represented as a tuple
     # makes a request to the perfect pentago API
     # returns the best response in the format of a decimal int
@@ -27,14 +29,16 @@ def makeRequest(state):
     movesDict = r.json()
 
     firstMove = chooseMove(movesDict, 'first')
+    print(firstMove)
 
     newURL = "https://backend.perfect-pentago.net/{}".format(firstMove)
 
     m = get(url=newURL, params=PARAMS)
     newMovesDict = m.json()
 
-    finalMove = chooseMove(movesDict, 'second')
-
+    finalMove = chooseMove(newMovesDict, 'second')
+    print("final")
+    print(finalMove)
     return finalMove
 
 
@@ -43,26 +47,26 @@ def chooseMove(movesDict, move):
     bestState = None
     if move == 'first':
         best = -2
+        bestMoves = []
         for state in movesDict:
             if state[-1] == 'm' and movesDict[state] > best:
-                bestState = state
                 best = movesDict[state]
-            elif state[-1] == 'm' and movesDict[state] == best and randint(0, 3) == 1:
-                bestState = state
+                bestMoves = [state]
+            elif state[-1] == 'm' and movesDict[state] == best:
                 best = movesDict[state]
-            if best == 1:
-                break
+                bestMoves.append(state)
 
     elif move == 'second':
         best = 2
+        bestMoves = []
         for state in movesDict:
-            if type(movesDict[state]) == int and int(movesDict[state]) < best:
-                bestState = state
+            if type(movesDict[state]) == int and int(movesDict[state]) < best and state[-1] != 'm':
                 best = movesDict[state]
-            elif type(movesDict[state]) == int and int(movesDict[state]) == best and randint(0, 3) == 1:
-                bestState = state
+                bestMoves = [state]
+            elif type(movesDict[state]) == int and int(movesDict[state]) == best and state[-1] != 'm':
                 best = movesDict[state]
-            if best == -1:
-                break
-
-    return bestState
+                bestMoves.append(state)
+    if bestMoves:
+        return choice(bestMoves)
+    else:
+        raise Exception()
